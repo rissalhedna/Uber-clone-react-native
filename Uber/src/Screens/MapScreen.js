a485b85d3b3b190521d2f166c3bbda5f61707e72
rissalhedna <rissalhedna@outlook.com>
2022-06-26 16:28:19 +0100

    Final modifications, menu icon

--- Uber/src/Screens/MapScreen.js
+++ Uber/src/Screens/MapScreen.js
@@ -1,15 +1,43 @@
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Icon } from "react-native-elements";
 import React from "react";
 import Map from "../Components/Map";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavigateCard from "../Components/NavigateCard";
 import RideOptionsCard from "./RideOptionsCard";
+import { useNavigation } from "@react-navigation/native";
 //DONT FORGET THIS
 //yarn add react-native-maps
 const Stack = new createNativeStackNavigator();
+
 const MapScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={{
+          position: "absolute",
+          top: 50,
+          left: 20,
+          zIndex: 1,
+          backgroundColor: "white",
+          padding: 10,
+          borderRadius: 50,
+          shadowColor: "black",
+          shadowOpacity: 0.2,
+          shadowOffset: { height: 2 },
+        }}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={{ height: "50%" }}>
         <Map />
       </View>
